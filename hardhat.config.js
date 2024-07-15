f4be80deebffcf5cd027696b451e6c29eb01c91c
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2024-07-15 14:31:38 -0700

    added networks on hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,15 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://sepolia.mode.network",
+        chainId: 919,
+      },
+    },
+    localhost: {
+      timeout: 16000000,
+    },
+  },
 };
